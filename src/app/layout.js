485edb44cf4c6aede8ac87ba248786f88122a272
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2024-09-29 11:39:24 +0100

    cover up orang in package.json

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "$ORANG",
-  description: "ORANG GANG",
+  title: "STEALTH LAUNCH",
+  description: "STEALTH LAUNCH",
 };
 
 export default function RootLayout({ children }) {
